9bd9e1fd66d3a46998b1c5fe70c57fb36345b64a
kkmjkim <44939907+kkmjkim@users.noreply.github.com>
2018-11-18 18:17:34 +0900

    Add files via upload

---
+++ MJ_4673.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+//¼¿ÇÁ³Ñ¹ö4673 ±è¹ÎÁø
+
+int d(int num) {
+
+	int temp = num;
+	int sum = temp;
+	int digit;
+
+	while (temp != 0) {
+		digit = temp % 10;
+		temp /= 10;
+		sum += digit;
+	}
+
+	return sum;
+}
+
+int main() {
+	const int MAX = 100;
+	int ary[MAX] = { 0 };
+
+	for (int i = 1; i <= MAX; i++) {
+		if (ary[i - 1] == 0) {
+			int output = d(i);
+
+			while (output <= MAX) {
+				ary[output - 1] = output;
+				output = d(output);
+			}
+		}
+	}
+
+	for (int j = 0; j < MAX; j++) {
+		if (ary[j] == 0)
+			cout << j + 1 << endl;
+	}
+
+	system("pause");
+	return 0;
+}
